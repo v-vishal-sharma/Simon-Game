b3feadf04e581503f05b96cbb7eb317865168887
vishal sharma <vishals200415@gmail.com>
2021-10-19 16:50:01 +0530

    .

--- game.js
+++ game.js
@@ -1,6 +1,12 @@
+var gamePattern = [];
+
 var buttonColors = ["red" , "blue" , "green" , "yellow"];
 
 function nextSequence() {
   randomNumber = Math.floor(Math.random()*4);
   return randomNumber;
 }
+
+var randomChosenColor = buttonColors[nextSequence()];
+
+gamePattern.push(randomChosenColor);
