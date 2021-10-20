085d6bdcdd5e8d7cf3ba4ac6415dd2b69fc22ad1
vishal sharma <vishals200415@gmail.com>
2021-10-20 18:10:26 +0530

    .

--- game.js
+++ game.js
@@ -18,5 +18,22 @@ $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
 $("#" + randomChosenColor).on("click" , function(){
   var au = new Audio("sounds/" + randomChosenColor + ".mp3");
   au.play();
-  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColor).addClass("pressed");
+  setTimeout(function(){$("#" + randomChosenColor).removeClass("pressed")} , 100);
 });
+
+var userClickedPattern = [];
+
+$(".btn").click(handler);
+
+function handler() {
+  var userChosenColor = $(".btn").attr("id");
+  return userChosenColor;
+}
+
+
+var choColor = handler();
+
+userClickedPattern.push(choColor);
+
+console.log(userClickedPattern);
